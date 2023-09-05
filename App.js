fced8fe696c5952d43a7d5ba89df448e4ae86c11
dipankarsahoo180 <dipankarsahoo180@gmail.com>
2023-09-05 05:33:55 +0530

    Episode-01-Inception

---
+++ App.js
@@ -0,0 +1,22 @@
+//create an element-simillar to document.createElement
+/**
+ * React.createElement(object) =>HTML (browser understands)
+ * React.createElement creates and object
+ * While it is rendering into DOM it converts the object into html
+ * param 1 - type: element name
+ * param 2 - props: attributes
+ * param 3 - children inside props: text of the element
+ */
+const h1 = React.createElement("h1",{xyz:'abc',id:'heading'},"hello World from React!");
+const h2 = React.createElement("h2",{xyz:'abc',id:'heading2'},"This is a h2 element");
+
+// Create nested Child using React.
+
+//add multiple child inside the element using array (siblings)
+const child = React.createElement('div',{id:'child'},['child div',h1,h2]); 
+const parent = React.createElement('div',{id:'parent'},child); 
+
+//ReactDOM is for dom interaction, Make #root as the root element of react
+const root = ReactDOM.createRoot(document.getElementById("root"));
+//render the element inside root
+root.render(parent);
