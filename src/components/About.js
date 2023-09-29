ceeea0f607a4ea51128622b2a4add403a7ff9117
dipankarsahoo180 <dipankarsahoo180@gmail.com>
2023-09-29 15:52:25 +0530

    Episode-10-Jo-dikhta-hai-vo-bikta-hai

--- src/components/About.js
+++ src/components/About.js
@@ -26,7 +26,7 @@ class About extends React.Component {
         return (
             <>
                 <h1>About</h1>
-                <div className="about-card">
+                <div className="flex flex-wrap justify-evenly">
                         <UserClass
                             name="Dipankar (Class Based)"
                             location="Bangalore(Class)"
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,24 +6,24 @@ const Header = () => {
     const [jsxButton,setJsxButton] = useState('Login')
     return (
         <>
-            <div className="header">
+            <div className="flex flex-wrap justify-between bg-gray-300 mb-2">
                 <Link to='/'>
-                    <div className="logo">
+                    <div className="w-40">
                     <img src={logo}></img>
                     </div>
                 </Link>    
-                <div className="nav-items">
-                    <ul>
-                        <li><Link className="link-class" to=''>Home</Link></li>
-                        <li><Link className="link-class" to='about'>About us</Link></li>
-                        <li><Link className="link-class" to='contact-us'>Contact us</Link></li>
-                        <li><Link className="link-class" to='cart'>Cart</Link></li>
-                        <li><Link className="link-class" to='grocery'>Grocery</Link></li>
+                <div className="">
+                    <ul className="flex flex-wrap justify-between p-4">
+                        <li><Link className="p-2 text-lg" to=''>Home</Link></li>
+                        <li><Link className="p-2 text-lg" to='about'>About us</Link></li>
+                        <li><Link className="p-2 text-lg" to='contact-us'>Contact us</Link></li>
+                        <li><Link className="p-2 text-lg" to='cart'>Cart</Link></li>
+                        <li><Link className="p-2 text-lg" to='grocery'>Grocery</Link></li>
                         {
                             (jsxButton == 'Login')?
-                            <li><button className="log-in-out-btn" onClick={()=>setJsxButton('Logout')}>Login</button></li>
+                            <li><button className="mx-2 rounded-sm px-2 py-1 bg-green-500" onClick={()=>setJsxButton('Logout')}>Login</button></li>
                             :
-                            <li><button className="log-in-out-btn" onClick={()=>setJsxButton('Login')}>Logout</button></li>
+                            <li><button className="mx-2 rounded-sm px-2 py-1 bg-green-500" onClick={()=>setJsxButton('Login')}>Logout</button></li>
                         }
                     </ul>
                 </div>
